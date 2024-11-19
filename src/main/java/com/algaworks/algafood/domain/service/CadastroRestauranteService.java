dd215d506bb2eec61b863d7d835e4108533b0c8a
Mormysh <leonardo.paascoal@gmail.com>
2024-11-19 19:08:57 -0300

    19:08 - 19/11

--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -2,11 +2,9 @@ package com.algaworks.algafood.domain.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.RestauranteNaoEncontradoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
-import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
 
@@ -15,9 +13,6 @@ public class CadastroRestauranteService {
     
     @Autowired
     private RestauranteRepository restauranteRepository;
-
-    @Autowired
-    private CozinhaRepository cozinhaRepository;
 
     @Autowired
     private CadastroCozinhaService cadastroCozinhaService;
