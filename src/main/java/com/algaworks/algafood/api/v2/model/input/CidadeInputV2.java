49655039f2a8d0b0898d000fec3b719c115025e8
Mormysh <leonardo.paascoal@gmail.com>
2025-01-20 15:12:47 -0300

    aula 21.1

--- src/main/java/com/algaworks/algafood/api/v2/model/input/CidadeInputV2.java
+++ src/main/java/com/algaworks/algafood/api/v2/model/input/CidadeInputV2.java
@@ -1,14 +1,16 @@
 package com.algaworks.algafood.api.v2.model.input;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+@ApiModel("CidadeInput")
 @Getter
 @Setter
-public class CidadeIdInputV2 {
+public class CidadeInputV2 {
     
     @ApiModelProperty(example = "Uberlândia", required = true)
     @NotBlank
--- src/main/java/com/algaworks/algafood/api/v2/model/input/CozinhaInputV2.java
+++ src/main/java/com/algaworks/algafood/api/v2/model/input/CozinhaInputV2.java
@@ -1,11 +1,13 @@
 package com.algaworks.algafood.api.v2.model.input;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 
+@ApiModel("CozinhaInput")
 @Setter
 @Getter
 public class CozinhaInputV2 {
